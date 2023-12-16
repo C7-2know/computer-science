ad1465a5194808636ca7b7dc65eb047950e8ca4f
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2023-12-16 06:32:44 +0300

    Added solution for Design Authentication Manager

--- G5/week1/design-authentication-manager.py
+++ G5/week1/design-authentication-manager.py
@@ -4,22 +4,17 @@ class AuthenticationManager:
     def __init__(self, timeToLive: int):
         self.time_live=timeToLive
         self.tokens={}
-        self.expire={}
-
+        
     def generate(self, tokenId: str, currentTime: int) -> None:
         self.tokens[tokenId]=currentTime+self.time_live
-        self.expire[currentTime]=self.expire.get(currentTime,0)+1
     def renew(self, tokenId: str, currentTime: int) -> None:
         if tokenId in self.tokens:
             if self.tokens[tokenId]>currentTime:
-                print(self.tokens[tokenId])
                 self.tokens[tokenId]=self.time_live + currentTime
-                
 
     def countUnexpiredTokens(self, currentTime: int) -> int:
         unexpired=0
         for tok,tim in self.tokens.items():
-            
             if tim>currentTime:
                 unexpired+=1
         return unexpired
