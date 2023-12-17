4491ebf3be64a3f603d6133cb8f15a12c0c0146b
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2023-12-17 04:55:22 +0300

    Added solution for Count of Matches in Tournament

--- G5/week1/count-of-matches-in-tournament.py
+++ G5/week1/count-of-matches-in-tournament.py
@@ -1,13 +1,20 @@
+import math
 class Solution:
     def numberOfMatches(self, n: int) -> int:
         total_matches=0
         while n>1:
-            match=0
-            if n%2==0:
-                match=n/2
-                n=n/2
-            else:
-                match=(n-1)/2
-                n=(n+1)/2
-            total_matches+=match
+            total_matches+=math.floor(n/2)
+            n=math.ceil(n/2)
         return int(total_matches)
+        
+        
+        # while n>1:
+        #     match=0
+        #     if n%2==0:
+        #         match=n/2
+        #         n=n/2
+        #     else:
+        #         match=(n-1)/2
+        #         n=(n+1)/2
+        #     total_matches+=match
+        # return int(total_matches)
