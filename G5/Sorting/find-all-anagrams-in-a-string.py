ff02a2694674d723550582230355ac5794aa0960
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2024-01-08 16:58:29 +0300

    Added solution for Find All Anagrams in a String

--- G5/Sorting/find-all-anagrams-in-a-string.py
+++ G5/Sorting/find-all-anagrams-in-a-string.py
@@ -10,13 +10,24 @@ class Solution:
         ind=[]
         if dp==ds:
             ind.append(0)
-        for i in range(1,len(s)-(k-1)):
-            # remove the first
-            if ds[s[i-1]]>1:
-                ds[s[i-1]]-=1
+        for i in range(k,len(s)):
+            if ds[s[i-k]]==1:
+                ds.pop(s[i-k])
             else:
-                ds.pop(s[i-1])
-            ds[s[i+k-1]]=ds.get(s[i+k-1],0)+1
+                ds[s[i-k]]-=1
+            ds[s[i]]=ds.get(s[i],0)+1
+            # print(ds,dp)
             if ds==dp:
-                ind.append(i)
+                ind.append(i-k+1)
         return ind
+        # for i in range(1,len(s)-(k-1)):
+        #     # remove the first element of the previous window
+        #     if ds[s[i-1]]>1:
+        #         ds[s[i-1]]-=1
+        #     else:
+        #         ds.pop(s[i-1])
+        #     # add the last element of the window
+        #     ds[s[i+k-1]]=ds.get(s[i+k-1],0)+1
+        #     if ds==dp:
+        #         ind.append(i)
+        # return ind
