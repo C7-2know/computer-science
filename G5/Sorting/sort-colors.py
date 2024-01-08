08fa00d3f607656ee9d33215864a32180c7a3c8c
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2024-01-08 16:40:35 +0300

    Added solution for Sort Colors

--- G5/Sorting/sort-colors.py
+++ G5/Sorting/sort-colors.py
@@ -3,11 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for k in range(i,0,-1):
-                if i==3:
-                    print(k)
-                    print(nums)
+        # for i in range(len(nums)):
+        #     for k in range(i,0,-1):
+        #         if i==3:
+        #             print(k)
+        #             print(nums)
+        #         if nums[k]<nums[k-1]:
+        #             nums[k],nums[k-1]=nums[k-1],nums[k]
 
-                if nums[k]<nums[k-1]:
-                    nums[k],nums[k-1]=nums[k-1],nums[k]
+        left=0
+        while left<len(nums):
+            right=len(nums)-1
+            while right>left:
+                if nums[left]>nums[right]:
+                    nums[left],nums[right]=nums[right],nums[left]
+                right-=1
+            left+=1
+        
+        
