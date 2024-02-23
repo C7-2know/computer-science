fab1a5a9fd9c0e85d3755d7d5f7b36c00890b46d
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2024-02-23 05:03:50 +0300

    Add solution for Partition Labels

---
+++ G5/week3/leetcode/partition-labels.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_index={}
+        for i in range(len(s)):
+            last_index[s[i]]=i
+        arr=[]
+        max_=last_index[s[0]]
+        for j in range(len(s)):
+            max_=max(last_index[s[j]],max_)
+            if j==max_:
+                if len(arr)==0:
+                    arr.append(max_+1)
+                else:
+                    arr.append(max_-sum(arr)+1)
+            
+            
+        return arr
+            
