ebe749e166fae45f6082960970ba98a4abd45de7
Betelhem Yimam <101956752+C7-2know@users.noreply.github.com>
2024-03-04 06:15:47 +0300

    Add solution for Power of Three

--- G5/week3/leetcode/power-of-three.py
+++ G5/week3/leetcode/power-of-three.py
@@ -1,9 +1,7 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        print(n)
         if n==1.0:
             return True
         if n<1:
-            print(n)
             return False
         return self.isPowerOfThree(n/3)
