d0e9c5b227facabdeb9d7c6ec983b4562a2d9725
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-05-07 02:21:30 +0300

    Time: 62 ms (44.27%), Space: 18.6 MB (82.88%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,15 +1,17 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        print(bin(~2))
-        print(bin(~1),bin(1))
-        nums.sort()
-        i=0
-        while i <len(nums)-1:
-            print(nums[i])
-            if nums[i]==nums[i+1]:
-                i+=3
-            else:
-                return nums[i]
+        # nums.sort()
+        # i=0
+        # while i <len(nums)-1:
+        #     if nums[i]==nums[i+1]:
+        #         i+=3
+        #     else:
+        #         return nums[i]
         
-        return nums[i]
-        # return 2
+        # return nums[i]
+
+        once,twice=0,0
+        for i in nums:
+            once=(once^i)&(~twice)
+            twice=(twice^i)&(~once)
+        return once
