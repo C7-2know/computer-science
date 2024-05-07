6892d1367165feb6fc8e5ba4b374b749da1b3602
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-05-07 04:02:09 +0300

    Time: 557 ms (20.49%), Space: 18.1 MB (29.22%) - LeetHub

---
+++ 0753-open-the-lock/0753-open-the-lock.py
@@ -0,0 +1,29 @@
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        deadends=set(deadends)
+        queue=[target]
+        direction=[1,-1]
+        visited=set([target])
+        opr=0
+        while queue:
+            size=len(queue)
+            # print("queue",queue,opr)
+            for i in range(size):
+                cur=queue[i]
+                if cur=="0000":
+                    return opr
+                for k in range(4):
+                    for ch in direction:
+                        num=ch+int(cur[k])
+                        if num>9:
+                            num=0
+                        elif num<0:
+                            num=9
+                        new=cur[:k]+str(num)+cur[k+1:]
+                        if new not in deadends and new not in visited :
+                            queue.append(new)
+                            visited.add(new)
+                            # print(new)
+            queue=queue[i+1:]
+            opr+=1
+        return -1
