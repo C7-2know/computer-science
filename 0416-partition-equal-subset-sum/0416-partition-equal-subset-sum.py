0b31571929df4e8f45788550e383846e1e2045dd
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-05-13 19:53:01 +0300

    Time: 926 ms (41.58%), Space: 214.3 MB (10.3%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,12 +1,18 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         memo={}
+        nums.sort()
+        count=0
         def dp(i,half_sum):
+            nonlocal count
+            count+=1
             if i>=len(nums):
                 return half_sum==0
             if (i,half_sum) not in memo:
                 memo[(i,half_sum)]=dp(i+1,half_sum-nums[i]) or dp(i+1,half_sum)
             return memo[(i,half_sum)]
+        
         if sum(nums)%2!=0:
             return False
-        return dp(1,sum(nums)//2)
+        soln=dp(1,sum(nums)//2)
+        return soln
