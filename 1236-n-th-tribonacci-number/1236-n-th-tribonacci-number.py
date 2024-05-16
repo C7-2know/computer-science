6d4db73637da5766b41aa9c7cfcbdcb6e0dfe78b
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-05-16 18:38:03 +0300

    Time: 30 ms (82.82%), Space: 16.5 MB (84.28%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,12 +1,18 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo={}
-        def rec(n):
-            if n<2:
-                return n
-            if n==2:
-                return 1
-            if n not in memo:
-                memo[n]=rec(n-1)+rec(n-2)+rec(n-3)
-            return memo[n]
-        return rec(n)
+        # memo={}
+        # def rec(n):
+        #     if n<2:
+        #         return n
+        #     if n==2:
+        #         return 1
+        #     if n not in memo:
+        #         memo[n]=rec(n-1)+rec(n-2)+rec(n-3)
+        #     return memo[n]
+        # return rec(n)
+
+        #Bottom up
+        arr=[0,1,1]
+        for i in range(3,n+1):
+            arr.append(arr[i-3]+arr[i-1]+arr[i-2])
+        return arr[n]
