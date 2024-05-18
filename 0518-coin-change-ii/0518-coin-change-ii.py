321bf644b2a241116f67362184ed938f5a05178d
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-05-18 06:52:28 +0300

    Time: 114 ms (76.74%), Space: 16.7 MB (95.13%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo={}
-        def dp(i,amt):
-            if i>=len(coins) or amt<0:
-                return 0
-            if amt==0:
-                return 1
-            if (i,amt) not in memo:
-                memo[(i,amt)]=dp(i+1,amt)+dp(i,amt-coins[i])
-            return memo[(i,amt)]
+        # memo={}
+        # def dp(i,amt):
+        #     if i>=len(coins) or amt<0:
+        #         return 0
+        #     if amt==0:
+        #         return 1
+        #     if (i,amt) not in memo:
+        #         memo[(i,amt)]=dp(i+1,amt)+dp(i,amt-coins[i])
+        #     return memo[(i,amt)]
+        # return dp(0,amount)
+        dp=[0 for _ in range(amount+1)]
+        dp[0]=1
+
+        for coin in coins:
+            for i in range(1,amount+1):
+                if coin<=i:
+                    dp[i]+=dp[i-coin]
+            # print(dp)
+        return dp[amount]      
         
-        return dp(0,amount) 
