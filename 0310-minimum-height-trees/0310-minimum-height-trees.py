cb4c700efad41fdcf04d5e0c7e875c999f90e429
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-15 06:17:16 +0300

    Time: 399 ms (73.03%), Space: 26.2 MB (84.77%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.py
+++ 0310-minimum-height-trees/0310-minimum-height-trees.py
@@ -1,27 +1,27 @@
 class Solution:
-  def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-    if n == 1 or not edges:
-      return [0]
-
-    ans = []
-    graph = collections.defaultdict(set)
-
-    for u, v in edges:
-      graph[u].add(v)
-      graph[v].add(u)
-
-    for label, children in graph.items():
-      if len(children) == 1:
-        ans.append(label)
-
-    while n > 2:
-      n -= len(ans)
-      nextLeaves = []
-      for leaf in ans:
-        u = next(iter(graph[leaf]))
-        graph[u].remove(leaf)
-        if len(graph[u]) == 1:
-          nextLeaves.append(u)
-      ans = nextLeaves
-
-    return ans
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n==1:
+            return [0]
+        graph=[[] for _ in range(n)]
+        size=[0 for _ in range(n)]
+        for i in edges:
+            size[i[0]]+=1
+            size[i[1]]+=1
+            graph[i[0]].append(i[1])
+            graph[i[1]].append(i[0])
+        queue=deque()
+        height=0
+        for nei in range(len(graph)):
+            if len(graph[nei])==1:
+                queue.append(nei)
+        while queue:
+            if n<=2:
+                return queue
+            for i in range(len(queue)):
+                node=queue.popleft()
+                n-=1
+                for nei in graph[node]:
+                    size[nei]-=1
+                    if size[nei]==1:
+                        queue.append(nei)
+       
