7e79d549fe35aeb8264ea0f94a3bafe325f593f5
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-16 22:42:21 +0300

    Time: 2076 ms (26.91%), Space: 19.7 MB (63.52%) - LeetHub

---
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        bits_={}
+        for i in words:
+            bit_='0b'+('0'*26)
+            for l in i:
+                ind=ord(l)-97
+                bit_=bit_[:ind+2]+'1'+bit_[ind+3:]
+            bits_[i]=bit_
+        max_=0
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if int(bits_[words[i]],2)&int(bits_[words[j]],2)==0:
+                    max_=max(max_,len(words[i])*len(words[j]))
+
+        return max_
+
