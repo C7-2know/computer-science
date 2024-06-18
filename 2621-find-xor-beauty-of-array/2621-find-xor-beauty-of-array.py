76494d77c7cd9d4885759e4c0f16d822ee302e67
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-18 10:27:43 +0300

    Time: 297 ms (7.58%), Space: 30.9 MB (87.08%) - LeetHub

--- 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.py
+++ 2621-find-xor-beauty-of-array/2621-find-xor-beauty-of-array.py
@@ -3,8 +3,4 @@ class Solution:
         res=0
         for i in nums:
             res^=i
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         for k in range(len(nums)):
-        #             res^=((nums[i] | nums[j])& nums[k])
         return res
