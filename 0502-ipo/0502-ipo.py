9cc63918697db07675cb6abf3f0c99d74e03b718
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-19 22:50:11 +0300

    Time: 740 ms (37.26%), Space: 41.1 MB (81.14%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -3,23 +3,16 @@ class Solution:
         profits=[-1*i for i in profits]
         c_p=list(zip(capital,profits))
         c_p.sort()
-        # heapify(c_p)
         poss=[]
         heapify(poss)
         ind=0
-        # print(c_p)
         while k and len(c_p)>0:
             while ind<len(c_p) and w>=c_p[ind][0]:
                 heappush(poss,c_p[ind][1])
                 ind+=1
-            # print(poss,w)
             if poss:
                 p=heappop(poss)
-                # print(w)
-
                 w+=(-1*p)
-                # print(w)
-            
             k-=1
         while k and poss:
             p=heappop(poss)
