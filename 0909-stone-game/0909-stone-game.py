2dbba28a1d85265f6f4ad7e147c3fb1a7342f5cd
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-23 23:31:37 +0300

    Time: 36 ms (87.17%), Space: 16.6 MB (73.33%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,6 +1,7 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
         def dp(i,alice,bob):
+            print(i,alice,bob)
             if i>=len(piles):
                 return alice>bob
             return dp(i+1,alice+piles[i],bob) or dp(i+1,alice,bob+piles[i])
