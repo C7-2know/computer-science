1262bd68c5974b01325fb2a321a8b119d1ce714d
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-06-29 00:08:14 +0300

    Time: 262 ms (96.06%), Space: 19.6 MB (43.78%) - LeetHub

---
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        total=0
+        p1=len(difficulty)-1
+        p2=len(worker)-1
+        worker.sort()
+        prof=list(zip(difficulty,profit))
+        prof.sort(key=lambda x:x[1])
+        # print(prof)
+        while p2>=0 and p1>=0:
+            # print(difficulty[p1],worker[p2])
+            if prof[p1][0]>worker[p2]:
+                p1-=1
+            else:
+                total+=prof[p1][1]
+                p2-=1
+        return total
