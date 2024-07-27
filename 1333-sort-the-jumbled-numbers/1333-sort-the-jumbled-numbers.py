1410141ddbf27c47a52cbbd874d7c0c9c4012272
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-07-27 23:59:51 +0300

    Time: 1062 ms (51.89%), Space: 24.9 MB (40.48%) - LeetHub

---
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortJumbled(self, mapping: List[int], nums: List[int]) -> List[int]:
+        arr2=[]
+        for num in nums:
+            n=''
+            for i in str(num):
+                n+=str(mapping[int(i)])
+            num=n
+            arr2.append(int(n))
+        zipd=list(zip(nums,arr2))
+        zipd.sort(key=lambda x:x[1])
+        res=[i[0] for i in (zipd)]
+        return res
+
