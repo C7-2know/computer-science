8c3aa8b37068ede1a9b3f6660e93c094a93c3a53
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-08-06 23:25:27 +0300

    Time: 587 ms (46.87%), Space: 31 MB (61.22%) - LeetHub

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/3249-minimum-number-of-operations-to-make-array-xor-equal-to-k.py
@@ -4,13 +4,11 @@ class Solution:
         for i in nums[1:]:
             xor^=i
         org_len=xor.bit_length()
-        # if xor<k:
-        #     return xor.bit_length()-k.bit_length()
         count=0
         xor^=k
         b_xor=bin(xor)
         for i in b_xor:
             if i=='1':
                 count+=1
-
+        print(count)
         return count
