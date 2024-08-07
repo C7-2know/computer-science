9284bce3c0f08a030516244d8b69d7d274d49b13
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-08-07 21:49:44 +0300

    Time: 128 ms (11.92%), Space: 17 MB (42.05%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
@@ -1,15 +1,14 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
-        val=[]
         memo={}
-        def dp(i,j):
-            if abs(i-j)<=1:
+        def dp(l,r):
+            if abs(l-r)<=1:
                 return 0
-            if (i,j) not in memo:
+            if (l,r) not in memo:
                 ans=float('inf')
-                for n in range(i+1,j):
-                    ans=min(ans,dp(i,n)+dp(n,j)+values[i]*values[j]*values[n])
-                memo[(i,j)]=ans
-            return memo[(i,j)]
-        ans=dp(0,len(values)-1)
-        return ans
+                for i in range(l+1,r):
+                    ans=min(ans,dp(l,i)+dp(i,r)+values[l]*values[i]*values[r])
+                memo[(l,r)]=ans
+            return memo[(l,r)]
+        
+        return dp(0,len(values)-1)
