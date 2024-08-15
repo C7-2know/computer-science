c876deb8155cf1c0a73b255e4ea185e7f2023887
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-08-16 00:09:11 +0300

    Time: 67 ms (46.78%), Space: 16.6 MB (39.6%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.py
+++ 0755-reach-a-number/0755-reach-a-number.py
@@ -1,12 +1,14 @@
 class Solution:
     def reachNumber(self, target: int) -> int:
-        steps=0
-        sum_=0
+        moves=0
+        st=0
         target=abs(target)
-        while sum_<target:
-            steps+=1
-            sum_+=steps
-        while (sum_-target)%2!=0:
-            steps+=1
-            sum_+=steps
-        return steps
+        while st<target:
+            moves+=1
+            st+=moves
+        while (st-target)%2!=0:
+            moves+=1
+            st-=moves
+            
+        return moves
+        
