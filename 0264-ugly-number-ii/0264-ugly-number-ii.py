118fe4796cd0f64ff998024648b7183730323b5e
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-08-28 22:49:58 +0300

    Time: 1032 ms (5%), Space: 17.6 MB (5.25%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        nums=[2,3,5]
+        ans=set([1])
+        
+        while len(ans)//4<n:
+            ls=list(ans)
+            for j in nums[::-1]:
+                for i in ls:
+                    ans.add(j*i)
+        ans=list(ans)
+        ans.sort()
+        return (ans[n-1])
+
+
+        
