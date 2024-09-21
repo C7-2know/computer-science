e1201e4774711ec087e0ce4ae11b1ac6ff042b90
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-09-21 21:08:13 +0300

    Time: 176 ms (8.31%), Space: 35.5 MB (5.07%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,44 +1,41 @@
-class TrieNode:
-    def __init__(self):
-        self.is_end=False
-        self.children=[None for _ in range(26)]
 class Trie:
 
     def __init__(self):
-        self.root=TrieNode()
+        self.children=[None for _ in range(26)]    
+        self.is_end=False    
 
     def insert(self, word: str) -> None:
-        cur=self.root
-        for i in word:
-            ind=ord(i)-ord('a')
-            if cur.children[ind]==None:
-                cur.children[ind]=TrieNode()
+        cur=self
+        for l in word:
+            ind=ord(l)-97
+            if not cur.children[ind]:
+                cur.children[ind]=Trie()
             cur=cur.children[ind]
         cur.is_end=True
-    
 
     def search(self, word: str) -> bool:
-        cur=self.root
-        inx=0
-        ind=ord(word[0])-ord("a")
-        while cur.children[ind]!=None and inx<len(word):
+        cur=self
+        for l in word:
+            ind=ord(l)-97
+            if cur.children[ind]==None:
+                return False
             cur=cur.children[ind]
-            inx+=1
-            if inx<len(word):
-                ind=ord(word[inx])-ord("a")
-        return cur.is_end and inx>=len(word)
+        else:
+            if cur.is_end:
+                return True
+            return False
+
         
 
     def startsWith(self, prefix: str) -> bool:
-        cur=self.root
-        i=0
-        ind=ord(prefix[i])-ord('a')
-        while cur.children[ind]!=None and i<len(prefix):
+        cur=self
+        for l in prefix:
+            ind=ord(l)-97
+            if cur.children[ind]==None:
+                return False
             cur=cur.children[ind]
-            i+=1
-            if i<len(prefix):
-                ind=ord(prefix[i])-ord('a')
-        return i==len(prefix)
+        return True
+
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
