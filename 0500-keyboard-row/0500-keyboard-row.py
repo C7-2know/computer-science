6a935700b1efdc6b3e2f2967903a0f68a94dac78
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-10-10 05:25:24 +0300

    Time: 35 ms (51.66%), Space: 16.7 MB (31.17%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        rows=[set([i for i in "qwertyuiop"])]
+        rows.append(set([i for i in "asdfghjkl"]))
+        rows.append(set([i for i in "zxcvbnm"]))
+        ans=[]
+        print(rows)
+        for w in words:
+            temp=w
+            w=w.lower()
+            row=0
+            if w[0] in rows[1]:
+                row=1
+            elif w[0] in rows[2]:
+                row=2
+            for l in w:
+                if l not in rows[row]:
+                    print(l,w,row)
+                    break
+            else:
+                ans.append(temp)
+        return ans
