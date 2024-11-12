9650fb0fb9d17e93d51a0fb14b98eb2ce6fa9fbb
Betelhem Yimam <betelhemyimamd@gmail.com>
2024-11-12 05:36:51 +0300

    Time: 99 ms (7.14%), Space: 17.1 MB (30%) - LeetHub

--- 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
+++ 1111-minimum-score-triangulation-of-polygon/1111-minimum-score-triangulation-of-polygon.py
@@ -7,8 +7,7 @@ class Solution:
             if (l,r) not in memo:
                 ans=float('inf')
                 for i in range(l+1,r):
-                    ans=min(ans,dp(l,i)+dp(i,r)+values[l]*values[i]*values[r])
+                    ans=min(ans,dp(l,i)+dp(i,r)+values[i]*values[l]*values[r])
                 memo[(l,r)]=ans
             return memo[(l,r)]
-        
         return dp(0,len(values)-1)
