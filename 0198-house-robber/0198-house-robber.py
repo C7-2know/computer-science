8298f6f7b4bc682dda574f378a65686a60566814
Betelhem Yimam <betelhemyimamd@gmail.com>
2025-12-15 20:17:02 +0300

    Time: 0 ms (100%), Space: 17.9 MB (12.16%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,34 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo={}
-        def dp(n):
-            if n>=len(nums):
+        memo = {}
+        def jump(i):
+            if i >=len(nums):
                 return 0
-            if n in memo:
-                return memo[n]
-            take = nums[n] + dp(n + 2)
-            dontake = dp(n + 1)
-            memo[n] = max(take,dontake)
-            return memo[n]
-        return dp(0)
-
-
-        # value=[(nums[1],nums[0],0)]
-        # set_={i:0 for i in range(len(nums))}
-        # for i in range(1,len(nums)-1):
-        #     value.append((nums[i-1]+nums[i+1],(nums[i]),i))
-        # value.append((nums[len(nums)-2],nums[len(nums)-1],len(nums)-1))
-        # money=0
-        # heapify(value)
-        # while set_:
-        #     lose,h,i=heappop(value)
-        #     print(i)
-        #     if i not in set_:
-        #         continue
-        #     if i-1 in set_:
-        #         set_.pop(i-1)
-        #     if i+1 in set_:
-        #         set_.pop(i+1)
-        #     set_.pop(i)
-        #     money+=h
-        # return money
+            if i in memo:
+                return memo[i]
+            take = nums[i] + jump(i+2)
+            n_take = jump(i+1)
+            memo[i] = n_take if n_take > take else take
+            return memo[i]
+        fs= jump(0)
+        sc= jump(1)
+        return fs if fs>sc else sc
