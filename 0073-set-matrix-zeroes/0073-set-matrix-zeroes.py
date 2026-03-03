8584964e0baba8e1e8d0180d95226c440c693d63
Betelhem Yimam <betelhemyimamd@gmail.com>
2026-03-03 23:39:11 +0300

    Time: 3 ms (68.55%), Space: 20.1 MB (38.46%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row,col=set(),set()
-        def is_valid(r,c):
-            return r>=0 and r<len(matrix) and c>=0 and c<len(matrix[0])
+        cols=set()
+        rows=set()
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]==0:
-                    row.add(i)
-                    col.add(j)
-                    for r in range(i):
-                        matrix[r][j]=0
-                    
-                    for c in range(j):
-                        matrix[i][c]=0
-                else:
-                    if i in row or j in col:
-                        matrix[i][j]=0
-                    
-        
-        
-        
+                    rows.add(i)
+                    cols.add(j)
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if i in rows:
+                    matrix[i][:]=[0]*len(matrix[i])
+                    break
+                if j in cols:
+                    matrix[i][j]=0
+
+
