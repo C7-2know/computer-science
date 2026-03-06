4e136f5d986b10aa7a572aa81943d7626d87681c
Betelhem Yimam <betelhemyimamd@gmail.com>
2026-03-07 00:42:40 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.50%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        i=0
+        while i < len(word) and word[i]!=ch:
+            i+=1
+        if i>=len(word):
+            return word
+        return word[:i+1][::-1]+word[i+1:]
