c3e78644ace436a08321ce6346d4036d7fcbc612
Betelhem Yimam <betelhemyimamd@gmail.com>
2026-05-12 10:12:54 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (38.31%) - LeetSync

---
+++ 1080-camelcase-matching/camelcase-matching.py
@@ -0,0 +1,19 @@
+class Solution:
+    def camelMatch(self, queries: List[str], pattern: str) -> List[bool]:
+        ans=[]
+        for q in queries:
+            p2=0
+            for c in q:
+                if p2<len(pattern) and c==pattern[p2]:
+                    p2+=1
+                elif ord(c)<91:
+                    print(c, p2, pattern)
+                    ans.append(False)
+                    break
+            else:
+                if  p2<len(pattern):
+                    ans.append(False)
+                else:
+                    ans.append(True)
+                
+        return ans
